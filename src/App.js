c88252297c276ee90eb28d166b80079ae89c8697
Sana Khan <mis.sanakhanam05@gmail.com>
2025-07-18 00:00:06 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MovieRevenueDemo from './components/MovieRevenueDemo';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MovieRevenueDemo />
+    </div>
+  );
+}
+
+export default App;
